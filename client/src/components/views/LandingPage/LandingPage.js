d454305a91fa251e9e105e4ea798a52af9dd36e7
Triplejblog <triplej03@naver.com>
2022-01-03 20:37:06 -0800

    Axios

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function LandingPage() {
+  useEffect(() => {
+    axios.get("/api/hello").then((response) => console.log(response.data));
+  }, []);
   return <div>시작 페이지</div>;
 }
 
