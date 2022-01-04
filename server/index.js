fd8907cf0ab0369f7772af827caa4803a08be28f
Triplejblog <triplej03@naver.com>
2022-01-03 20:45:11 -0800

    proxy, CORS

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
@@ -99,4 +98,5 @@ app.get("/api/hello", (req, res) => {
   res.send("hello~!");
 });
 
+const port = 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
