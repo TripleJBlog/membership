37047d2546e03ba1f612fce5ea064f9abf1ef1fd
Triplejblog <triplej03@naver.com>
2022-01-08 22:41:10 -0800

    login page

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import { useDispatch } from "react-redux";
 
 function LoginPage() {
-  return <div>This is a LoginPage</div>;
+  const dispatch = useDispatch();
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    dispatch(loginUser(body));
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler}></input>
+
+        <label>Password</label>
+        <input
+          type="password"
+          value={Password}
+          onChange={onPasswordHandler}
+        ></input>
+
+        <br></br>
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
