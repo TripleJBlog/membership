73e12a3863f889b45c0e830bed39886c5e5eb87e
Triplejblog <triplej03@naver.com>
2022-01-09 23:19:28 -0800

    logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,35 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
-  return <div>시작 페이지</div>;
+
+  const onClickHandler = () => {
+    axios.get(`api/users/logout`).then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to logout");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <div>시작 페이지</div>
+      <button onClick={onClickHandler}>logout</button>
+    </div>
+  );
 }
 
 export default LandingPage;
